9a38048bd8b802fed6dbaba3ad388331a59d6613
Cecília <130860320+mimistudies90@users.noreply.github.com>
2025-06-18 20:20:22 -0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <title>Página Inicial</title>
+</head>
+
+<body>
+
+    <h1>Bem-vindo!</h1>
+    <form action="login.php" method="GET">
+        <button type="submit">Login</button>
+    </form>
+    <br>
+    <form action="cadastro.php" method="GET">
+        <button type="submit">Cadastro</button>
+    </form>
+
+</body>
+
+</html>
